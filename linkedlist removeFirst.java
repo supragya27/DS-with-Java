51a61083fff07277fda2a303ab29e4cf717d39f9
supragya27 <supragya.chauhan001@gmail.com>
2020-01-28 18:39:57 +0530

    code to delete first element of a linked list

---
+++ linkedlist removeFirst.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+class Node{
+    int data; Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+    }
+class LinkedList{
+    Node head;
+    public LinkedList(){
+     head=null;
+    }
+    LinkedList insert(LinkedList l,int d){
+        Node newNode=new Node(d);
+        Node currentNode;
+        if(l.head==null)
+        l.head=newNode;
+        else{
+            currentNode=l.head;
+            while(currentNode.next!=null){
+                currentNode=currentNode.next;
+            }
+            currentNode.next=newNode;
+        } return l;
+    }
+    LinkedList removefirst(LinkedList l){
+        if(head==null)
+        {
+            System.out.println("Nothing to be deleted, list is empty.");
+            return l;
+        }
+        Node currentNode;
+        currentNode=l.head;
+        l.head=currentNode.next;
+        return l;
+    }
+    void display(LinkedList l){
+        Node newN=l.head;
+        if(head==null){
+            System.out.println("Empty list");
+            return;
+        }
+        else{
+        while(newN!=null){
+        System.out.println(newN.data+" "); newN=newN.next;}}
+    }
+}
+class Main
+{
+
+    public static void main(String[] args)
+    {
+       LinkedList l1=new LinkedList();    
+       l1=l1.insert(l1,15); 
+       l1=l1.removefirst(l1);
+       l1.display(l1);
+       l1=l1.removefirst(l1);
+       
+    }
+    }
