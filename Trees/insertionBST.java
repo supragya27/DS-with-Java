09a938d8cb2fb85c8391ee4c43f67298ec1dda32
supragya27 <supragya.chauhan001@gmail.com>
2020-03-24 18:51:48 +0530

    added the code for insertion in a binary search tree

---
+++ Trees/insertionBST.java
@@ -0,0 +1,29 @@
+public static Node insert(Node root,int data) {
+           if(root==null)
+          root=new Node(data);
+          else
+          {
+              Node cN=root;
+              while(true)
+              {
+                  if(cN.data>data&&cN.left==null)
+                  break;                  
+                  if(cN.data<data&&cN.right==null)
+                  break;
+                  if(data>cN.data)
+                  cN=cN.right;
+                  else
+                  cN=cN.left;
+              }              
+              if(data<cN.data)
+              {
+              cN.left=new Node(data);
+              }
+              else
+              cN.right=new Node(data);
+              
+           }
+
+          return root;
+    	
+    }
