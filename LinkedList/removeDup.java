884753e70b48eedbdddc381834fa89a2be50675c
supragya27 <supragya.chauhan001@gmail.com>
2020-03-27 16:29:06 +0530

    added the code to remove duplicates from a sorted Linkedlist

---
+++ LinkedList/removeDup.java
@@ -0,0 +1,15 @@
+Node removeDuplicates(Node head) {
+    if (head == null || head.next == null){
+        return head;
+    }
+    if (head.data == head.next.data)
+    {
+        head.next = head.next.next;
+        removeDuplicates(head);
+    }
+    else
+    {
+        removeDuplicates(head.next);
+    }
+    return head;
+}
